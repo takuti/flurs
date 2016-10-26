61584f2176b5379926eb6e3372ac863069f49e7f
Takuya Kitazawa <k.takuti@gmail.com>
2016-10-26 10:40:17 +0900

    Init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='recoil',
+    version='0.0.1',
+    description='A library for top-k recommendation based on implicit online learning',
+    author='Takuya Kitazawa',
+    author_email='k.takuti@gmail.com',
+    license='MIT',
+    url='https://github.com/takuti/recoil',
+    packages=['recoil'],
+    install_requires=[],
+)
