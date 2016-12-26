22a993b3dd1d014dfaf3d933a9b2b7d87fa29100
Takuya Kitazawa <k.takuti@gmail.com>
2016-12-26 17:58:40 +0900

    Update arguments for feature-hashing

--- flurs/utils/hash.py
+++ flurs/utils/hash.py
@@ -2,13 +2,13 @@ import mmh3
 import numpy as np
 
 
-def n_feature_hash(s, seeds, dims):
+def n_feature_hash(feature, dims, seeds):
     """N-hot-encoded feature hashing.
 
     Args:
-        s (str): Target string.
-        seeds (list of float): Seed of each hash function (mmh3).
+        feature (str): Target feature represented as string.
         dims (list of int): Number of dimensions for each hash value.
+        seeds (list of float): Seed of each hash function (mmh3).
 
     Returns:
         numpy 1d array: n-hot-encoded feature vector for `s`.
@@ -18,25 +18,25 @@ def n_feature_hash(s, seeds, dims):
     offset = 0
 
     for seed, dim in zip(seeds, dims):
-        vec[offset:(offset + dim)] = feature_hash(s, seed, dim)
+        vec[offset:(offset + dim)] = feature_hash(feature, dim, seed)
         offset += dim
 
     return vec
 
 
-def feature_hash(s, seed, dim):
+def feature_hash(feature, dim, seed=123):
     """Feature hashing.
 
     Args:
-        s (str): Target string.
-        seed (float): Seed of a MurmurHash3 hash function.
+        feature (str): Target feature represented as string.
         dim (int): Number of dimensions for a hash value.
+        seed (float): Seed of a MurmurHash3 hash function.
 
     Returns:
         numpy 1d array: one-hot-encoded feature vector for `s`.
 
     """
     vec = np.zeros(dim)
-    i = mmh3.hash(s, seed) % dim
+    i = mmh3.hash(feature, seed) % dim
     vec[i] = 1
     return vec
