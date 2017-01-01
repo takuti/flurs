9e81fa66c48e56bf237de9820d4682ed2f44728f
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-02 07:14:07 +0900

    Create tests for feature hashing

---
+++ flurs/utils/tests/test_hash.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+import numpy as np
+
+from flurs.utils.hash import n_feature_hash, feature_hash, multiple_feature_hash
+
+
+class HashTestCase(TestCase):
+
+    def test_feature_hash(self):
+        x = feature_hash("Tom", 5, seed=123)
+        nnz = np.nonzero(x)[0].size
+        self.assertEqual(nnz, 1)
+        self.assertEqual(x.size, 5)
+
+    def test_multiple_feature_hash(self):
+        x = multiple_feature_hash("Tom", 5, seed=123)
+        nnz = np.nonzero(x)[0].size
+        self.assertEqual(nnz, 1)
+        self.assertEqual(x.size, 5)
+
+    def test_n_feature_hash(self):
+        x = n_feature_hash("Tom", [5, 5, 5], seeds=[123, 456, 789])
+        nnz = np.nonzero(x)[0].size
+        self.assertEqual(nnz, 3)
+        self.assertEqual(x.size, 15)
