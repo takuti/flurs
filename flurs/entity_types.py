d99347b5edc29c983730678a54284ca60fec45c8
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-03 05:54:31 +0900

    Remove __str__ methods from entity types

--- flurs/entity_types.py
+++ flurs/entity_types.py
@@ -7,18 +7,12 @@ class User:
         self.index = index
         self.feature = feature
 
-    def __str__(self):
-        return 'user %d: %s' % (self.index, self.feature)
-
 
 class Item:
 
     def __init__(self, index, feature=np.array([0.])):
         self.index = index
         self.feature = feature
-
-    def __str__(self):
-        return 'item %d: %s' % (self.index, self.feature)
 
 
 class Event:
