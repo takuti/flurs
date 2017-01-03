56dc4faa6b8c39de5b197db0e9adc9b094335774
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-04 07:40:03 +0900

    Create tests for base classes

---
+++ flurs/tests/test_base.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from flurs.base import RecommenderMixin
+
+
+class RecommenderMixinTestCase(TestCase):
+
+    def setUp(self):
+        self.recommender = RecommenderMixin()
+
+    def test_init_recommender(self):
+        self.recommender.init_recommender()
+        self.assertEqual(self.recommender.n_user, 0)
+        self.assertEqual(self.recommender.users, {})
+        self.assertEqual(self.recommender.n_item, 0)
+        self.assertEqual(self.recommender.items, {})
+
+    def test_is_new_user(self):
+        self.recommender.init_recommender()
+
+        self.assertTrue(self.recommender.is_new_user(1))
+        self.assertTrue(self.recommender.is_new_user(2))
+
+        self.recommender.users[1] = {}
+        self.assertFalse(self.recommender.is_new_user(1))
+
+    def test_is_new_item(self):
+        self.recommender.init_recommender()
+
+        self.assertTrue(self.recommender.is_new_item(1))
+        self.assertTrue(self.recommender.is_new_item(2))
+
+        self.recommender.items[1] = {}
+        self.assertFalse(self.recommender.is_new_item(1))
+
+    def test_scores2recos(self):
+        candidates = np.array([10, 100, 1000])
+        scores = np.array([1., 5., 3.])
+
+        candidates_, scores_ = self.recommender.scores2recos(scores, candidates)
+        assert_array_equal(scores_, np.array([1., 3., 5.]))
+        assert_array_equal(candidates_, np.array([10, 1000, 100]))
+
+        candidates_, scores_ = self.recommender.scores2recos(scores, candidates, rev=True)
+        assert_array_equal(scores_, np.array([5., 3., 1.]))
+        assert_array_equal(candidates_, np.array([100, 1000, 10]))
