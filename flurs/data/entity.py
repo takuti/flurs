2001f3cff90f18e8dbc9e70879e82110f712f960
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-10 15:36:46 +0900

    Add encode() method to convert into feature vectors

--- flurs/data/entity.py
+++ flurs/data/entity.py
@@ -1,18 +1,44 @@
 import numpy as np
 
 
-class User:
+class Base:
 
     def __init__(self, index, feature=np.array([0.])):
         self.index = index
         self.feature = feature
 
+    def encode(self, dim=None,
+               index=True, feature=True,
+               vertical=False):
 
-class Item:
+        if not dim:
+            dim = self.index + 1
 
-    def __init__(self, index, feature=np.array([0.])):
-        self.index = index
-        self.feature = feature
+        x = np.array([])
+
+        if index:
+            x = np.concatenate((x, self.index_one_hot(dim)))
+
+        if feature:
+            x = np.concatenate((x, self.feature))
+
+        return x if not vertical else np.array([x]).T
+
+    def index_one_hot(self, dim):
+        if self.index >= dim:
+            raise ValueError('number of dimensions must be greater than index: %d' % self.index)
+
+        x = np.zeros(dim)
+        x[self.index] = 1.
+        return x
+
+
+class User(Base):
+    pass
+
+
+class Item(Base):
+    pass
 
 
 class Event:
@@ -22,3 +48,19 @@ class Event:
         self.item = item
         self.value = value
         self.context = context
+
+    def encode(self, n_user=None, n_item=None,
+               index=True, feature=True, context=True,
+               vertical=False):
+
+        x = self.user.encode(dim=n_user, index=index,
+                             feature=feature, vertical=False)
+
+        if context:
+            x = np.concatenate((x, self.context))
+
+        iv = self.item.encode(dim=n_item, index=index,
+                              feature=feature, vertical=False)
+        x = np.concatenate((x, iv))
+
+        return x if not vertical else np.array([x]).T
