a3d4ffbaf415d5034ca4ed8adae743c6f45f4172
Takuya Kitazawa <k.takuti@gmail.com>
2017-01-17 15:34:26 +0900

    Publish to PyPI

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 setup(
     name='flurs',
-    version='0.0.1',
-    description='A library for streaming recommender systems',
+    version='v0.0.1',
+    description='A library for online item recommendation',
     author='Takuya Kitazawa',
     author_email='k.takuti@gmail.com',
     license='MIT',
     url='https://github.com/takuti/flurs',
+    download_url='https://github.com/takuti/flurs/tarball/v0.0.1',
     packages=['flurs',
               'flurs.data',
               'flurs.recommender',
