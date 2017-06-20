38865ab304993a3c1dbe9ddddb7d8d3ff24ac03e
Takuya Kitazawa <k.takuti@gmail.com>
2017-06-19 23:38:12 -0700

    Rename version tag

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='flurs',
-    version='v0.0.1',
+    version='0.0.1',
     description='A library for online item recommendation',
     author='Takuya Kitazawa',
     author_email='k.takuti@gmail.com',
     license='MIT',
     url='https://github.com/takuti/flurs',
-    download_url='https://github.com/takuti/flurs/tarball/v0.0.1',
+    download_url='https://github.com/takuti/flurs/tarball/0.0.1',
     packages=['flurs',
               'flurs.data',
               'flurs.recommender',
